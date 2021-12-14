bb3347be0491b88dfa77840bedfdf4c9dbc63ae6
HLorenTo <lorena.torres.h@hotmail.com>
2021-12-13 20:50:22 -0500

    first commit

---
+++ src/evaluuacion/Evaluuacion.java
@@ -0,0 +1,17 @@
+
+package evaluuacion;
+
+
+public class Evaluuacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    for (int i=0; i < 100; i+=10){
+        System.out.println(i);
+    }
+    }
+    
+}
